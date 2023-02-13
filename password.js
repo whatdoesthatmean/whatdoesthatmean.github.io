6cd8fc718c3d6f273033f7ab8f7c5eb7a23a4841
whatdoesthatmean <119376960+whatdoesthatmean@users.noreply.github.com>
2023-02-13 11:51:49 -0800

    Update password.js

--- password.js
+++ password.js
@@ -24,7 +24,7 @@
     <p style="font-size: 30pt;"> TESTING THE PASSWORD</p>
     <p>Enter the password to view .</p>
     <p>Password:<input id="passwordBox" type="password"/></p>
-    <a href="monkdiamonddiscovery.html" onclick="return"
+    <a href="https://whatdoesthatmean.github.io/" onclick="return"
     CheckPassword();">
     click here to submit a pasword and view website
     </a>
